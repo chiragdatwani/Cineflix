fb8f1adab193ce39c19b31b3e7a6b04150ad5075
Chirag Datwani <chiragdatwani01@gmail.com>
2021-03-08 03:12:37 +0100

    added ScrollToTop component to scroll back to the top after changing routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-
+import ScrollToTop from "./components/ScrollToTop";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
